2cb7fa5a931b3d4918bf60cb7d75946c4236c86f
PCloudTest <1026543901@qq.com>
2013-12-25 01:38:48 -0800

    modified:   lib/language_pack/nginx_tomcat.rb

--- lib/language_pack/nginx_tomcat.rb
+++ lib/language_pack/nginx_tomcat.rb
@@ -23,7 +23,7 @@ module LanguagePack
 
     def compile
       Dir.chdir(build_path) do
-        install_java
+        # install_java
         install_nginx
         configure_nginx
         #install_java
@@ -34,7 +34,7 @@ module LanguagePack
         # install_database_drivers
         # #install_insight
         # copy_resources
-        setup_profiled
+        # setup_profiled
       end
     end
 
