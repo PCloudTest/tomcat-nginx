85343e203ad35ccfe1273be1d78565f1d7247cd1
PCloudTest <1026543901@qq.com>
2013-12-24 18:19:45 -0800

    modified:   bin/detect 	modified:   lib/language_pack.rb

--- lib/language_pack.rb
+++ lib/language_pack.rb
@@ -7,6 +7,7 @@ require "language_pack/spring"
 require "language_pack/grails"
 require "language_pack/play"
 require "language_pack/xml_wrapper"
+require "language_pack/nginx_tomcat"
 
 # General Language Pack module
 module LanguagePack
@@ -17,7 +18,7 @@ module LanguagePack
   def self.detect(*args)
     Dir.chdir(args.first)
 
-    pack = [ Play, Grails, Spring, JavaWeb, Java ].detect do |klass|
+    pack = [ Play, Grails, Spring, JavaWeb, Java , NginxTomcat ].detect do |klass|
       klass.use?
     end
 
