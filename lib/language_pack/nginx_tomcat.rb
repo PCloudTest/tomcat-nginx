6cce991d8feccb279d87cb70504676bc92c8b6f5
PCloudTest <1026543901@qq.com>
2013-12-25 02:14:20 -0800

    modified:   lib/language_pack/nginx_tomcat.rb

--- lib/language_pack/nginx_tomcat.rb
+++ lib/language_pack/nginx_tomcat.rb
@@ -105,13 +105,13 @@ module LanguagePack
       opts.delete("-Djava.io.tmpdir=")
       opts
     end
-    # def release
-    #   {
-    #       "addons" => [],
-    #       "config_vars" => {},
-    #       "default_process_types" => default_process_types
-    #   }.to_yaml
-    # end
+    def release
+      {
+          "addons" => [],
+          "config_vars" => {},
+          "default_process_types" => default_process_types
+      }.to_yaml
+    end
 
     def default_process_types
       {
