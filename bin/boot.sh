c2f96ee6b5e5ea1d1ceb0f107b863e1737d21826
PCloudTest <1026543901@qq.com>
2013-12-25 23:00:37 -0800

    modified:   bin/boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -12,24 +12,24 @@
 # and  limitations under the License.
 # ------------------------------------------------------------------------------------------------
 
-# export APP_ROOT=$HOME
+export APP_ROOT=$HOME
 
-# conf_file=$APP_ROOT/nginx/conf/nginx.conf
-# if [ -f $APP_ROOT/nginx.conf ]
-# then
-#   conf_file=$APP_ROOT/nginx.conf
-# fi
+conf_file=$APP_ROOT/nginx/conf/nginx.conf
+if [ -f $APP_ROOT/nginx.conf ]
+then
+  conf_file=$APP_ROOT/nginx.conf
+fi
 
-# mv $conf_file $APP_ROOT/nginx/conf/orig.conf
-# erb $APP_ROOT/nginx/conf/orig.conf > $APP_ROOT/nginx/conf/nginx.conf
-
-# # ------------------------------------------------------------------------------------------------
-
-# (tail -f -n 0 $APP_ROOT/nginx/logs/*.log &)
-# exec $APP_ROOT/nginx/sbin/nginx -p $APP_ROOT/nginx -c $APP_ROOT/nginx/conf/nginx.conf
+mv $conf_file $APP_ROOT/nginx/conf/orig.conf
+erb $APP_ROOT/nginx/conf/orig.conf > $APP_ROOT/nginx/conf/nginx.conf
 
 # ------------------------------------------------------------------------------------------------
 
-./bin/catalina.sh run
+(tail -f -n 0 $APP_ROOT/nginx/logs/*.log &)
+exec $APP_ROOT/nginx/sbin/nginx -p $APP_ROOT/nginx -c $APP_ROOT/nginx/conf/nginx.conf
+
+# ------------------------------------------------------------------------------------------------
+
+# ./bin/catalina.sh run
 
 # ------------------------------------------------------------------------------------------------
