0dc8e5d48fc27ce6d0f7825e4e9d392884c38f0c
PCloudTest <1026543901@qq.com>
2013-12-27 01:48:59 -0800

    modified:   bin/boot.sh 	modified:   lib/language_pack/nginx_tomcat.rb 	modified:   resources/tomcat/conf/server.xml

--- lib/language_pack/nginx_tomcat.rb
+++ lib/language_pack/nginx_tomcat.rb
@@ -132,7 +132,7 @@ module LanguagePack
 
     def default_process_types
       {
-        "web" => "sh boot.sh"
+        "web" => "./bin/catalina.sh run"
         # "web" => "exec nginx/sbin/nginx"
       }
     end
