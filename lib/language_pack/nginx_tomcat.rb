add63bda4adeec4f16cb1c159dfe6fa2bf2769d1
PCloudTest <1026543901@qq.com>
2013-12-29 22:28:20 -0800

    modified:   lib/language_pack/nginx_tomcat.rb 	modified:   resources/nginx/conf/nginx.conf

--- lib/language_pack/nginx_tomcat.rb
+++ lib/language_pack/nginx_tomcat.rb
@@ -98,7 +98,7 @@ module LanguagePack
 
     def copy_webapp_to_tomcat
       # run_with_err_output("mkdir -p #{tomcat_dir}/webapps/ROOT && mv * #{tomcat_dir}/webapps/ROOT")
-      run_with_err_output("cp -f index.jsp #{tomcat_dir}/webapps/ROOT")
+      run_with_err_output("cp -f index.html #{tomcat_dir}/webapps/ROOT && rm -fr #{tomcat_dir}/webapps/ROOT/index.jsp")
     end
 
     def move_tomcat_to_root
