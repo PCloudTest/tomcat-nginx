38bae614d64d3af525ccdf45a5144042e3e2d799
PCloudTest <1026543901@qq.com>
2013-12-30 19:29:16 -0800

    modified:   bin/boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -36,7 +36,10 @@ erb $APP_ROOT/nginx/conf/orig.conf > $APP_ROOT/nginx/conf/nginx.conf
 exec $APP_ROOT/nginx/sbin/nginx -p $APP_ROOT/nginx -c $APP_ROOT/nginx/conf/nginx.conf
 ./bin/catalina.sh run
 # ------------------------------------------------------------------------------------------------
-# dones
+# done
+while true
+
+done
 
 
 
