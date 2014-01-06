43fb8dbe25fe845034155a37f67706c610f74c63
PCloudTest <1026543901@qq.com>
2014-01-06 02:56:57 -0800

    modified:   lib/language_pack/nginx_tomcat.rb

--- lib/language_pack/nginx_tomcat.rb
+++ lib/language_pack/nginx_tomcat.rb
@@ -127,7 +127,7 @@ module LanguagePack
     def java_opts
       # TODO proxy settings?
       # Don't override Tomcat's temp dir setting
-      opts = super.merge({ "-Dhttp.port=" => "6700" })
+      opts = super.merge({ "-Dhttp.port=" => "6701" })
       opts.delete("-Djava.io.tmpdir=")
       opts
     end
