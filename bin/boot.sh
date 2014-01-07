1bdaa27d1f1eaedd3a3eb0ca03d62ac2e90a7a2e
PCloudTest <1026543901@qq.com>
2014-01-06 22:53:34 -0800

    modified:   bin/boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -33,13 +33,14 @@ erb $APP_ROOT/.nginx/conf/orig.conf > $APP_ROOT/.nginx/conf/nginx.conf
 
 
 
-./bin/startup.sh run && exec $APP_ROOT/.nginx/sbin/nginx -p $APP_ROOT/.nginx -c $APP_ROOT/.nginx/conf/nginx.conf
+./bin/startup.sh run 
+exec $APP_ROOT/.nginx/sbin/nginx -p $APP_ROOT/.nginx -c $APP_ROOT/.nginx/conf/nginx.conf
 
 # ------------------------------------------------------------------------------------------------
 # done
-# while true
+while true
 
-# done
+done
 
 
 
