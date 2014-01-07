f29ae4e2ad14a72f77773eec1e66e0d7f891557e
PCloudTest <1026543901@qq.com>
2014-01-06 17:47:56 -0800

    modified:   lib/language_pack/nginx_tomcat.rb

--- lib/language_pack/nginx_tomcat.rb
+++ lib/language_pack/nginx_tomcat.rb
@@ -114,7 +114,7 @@ module LanguagePack
       # run_with_err_output("cp -f *.html #{tomcat_dir}/webapps/ROOT && rm -fr #{tomcat_dir}/webapps/ROOT/index.jsp  && " +
       #   "mv css js images #{tomcat_dir}/webapps/ROOT/ && mv WEB-INF/web.xml #{tomcat_dir}/webapps/ROOT/WEB-INF")
     run_with_err_output("cp -f *.html #{tomcat_dir}/webapps/ROOT  && " +
-        "mv css js images #{tomcat_dir}/webapps/ROOT/ && mv WEB-INF #{tomcat_dir}/webapps/ROOT/")
+        "mv css js images WEB-INF #{tomcat_dir}/webapps/ROOT/")
     end
 
     def move_tomcat_to_root
